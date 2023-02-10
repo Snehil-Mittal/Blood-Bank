8302d8801fa2467e314f2e95c4d62e76fca73fbb
Nishu Aggrawal <76695085+Nishu-Aggrawal9027@users.noreply.github.com>
2023-02-10 22:09:44 +0530

    Add files via upload

--- Data/IUserDetailsRepository.cs
+++ Data/IUserDetailsRepository.cs
@@ -8,9 +8,12 @@ namespace BloodBankManagementSystem.Data
 {
     interface IUserDetailsRepository
     {
-        int InsertUser(UserDetails u);
-        int UpdateUserContactDetails(int userId, long mobile_no, string email);
-        int UpdateUserLocation(int userId, string location);
-        int DeleteUser(int userId);
+        Task<int> InsertUser(UserDetails u);
+        Task<int> UpdateUserContactDetails(int userId, long mobile_no, string email);
+        Task<int> UpdateUserLocation(int userId, string location);
+        Task<int> DeleteUser(int userId);
+        Task<UserDetails> GetUserById(int id);
+        Task<IEnumerable<UserDetails>> GetUsers();
+        Task<UserDetails> GetUserByBloodGroup(string bloodGroup);
     }
 }
