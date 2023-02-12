ffff27f6c93a7c2946e4470eea51709577b8f383
Nishu Aggrawal <76695085+Nishu-Aggrawal9027@users.noreply.github.com>
2023-02-12 16:17:11 +0530

    Add files via upload

--- Data/IUserDetailsRepository.cs
+++ Data/IUserDetailsRepository.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace BloodBankManagementSystem.Data
 {
-    public interface IUserDetailsRepository
+    interface IUserDetailsRepository
     {
-        Task<bool> InsertUser(UserDetails u);
-        Task<bool> UpdateUserDetails(UserDetails u);
-        Task<bool> UpdateUserProfile(int id, string role,bool availability);
-        Task<bool> DeleteUser(int userId);
-        Task<UserDetails> GetUserById(int id);
-        Task<IEnumerable<UserDetails>> GetUsers();
-        Task<IEnumerable<UserDetails>> GetUsersByBloodGroup(string bloodGroup);
+        Task<int> InsertUser(UserDetails u);
+        Task<int> UpdateUserContactDetails(int userId, long mobile_no, string email);
+        Task<int> UpdateUserLocation(int userId, string location);
+        Task<int> DeleteUser(int userId);
+        Task<ReadUserDto> GetUserById(int id);
+        Task<IEnumerable<ReadUserDto>> GetUsers();
+        Task<ReadUserDto> GetUserByBloodGroup(string bloodGroup);
     }
 }
